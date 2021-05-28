8bd0f675880901214991441a7ee10c5211cd213a
ntgato <ntgato@gmail.com>
2021-05-28 20:55:42 +0900

    모바일 뷰포트일때 각 영역의 이미지 교체, 하단 푸터 영역 구조 변경, 클릭 이벤트를 트리거로 하는 아코디언 메뉴 적용

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 동영상 제어
 const video = document.getElementById('wwdc21');
 const fig2 = document.querySelector('.wwdc21-post');
 
@@ -5,3 +6,16 @@ video.addEventListener('ended', function(end){
     fig2.classList.add('appear');
     video.classList.add('disappear');
 })
+
+// 하단 디렉토리 메뉴 모바일 대응
+const directory = document.querySelector('.label');
+
+function toggleMenu(id) {
+    const clicked = document.getElementById(id);
+    const target = clicked.nextSibling.nextSibling;
+    if(target.classList == "directory-items") {
+        target.classList.add('visible');
+    } else {
+        target.classList.remove('visible');
+    }
+}
